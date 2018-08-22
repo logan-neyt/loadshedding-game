90f7138405d710ce6052e5f781f573de317abc7f
Logan Neyt <33159068+logan-neyt@users.noreply.github.com>
2018-08-22 22:58:54 +0200

    Rewrote "Office" object
    It will be simpler and smaller to do without kontra sprites for this project.

--- src/game.js
+++ src/game.js
@@ -1,5 +1,8 @@
 kontra.init();  // Initilize the kontra library.
 
+var canvas = document.querySelector("canvas");
+var context = canvas.getContext("2d");
+
 var drawingScale = 5; // Distance between grid references used in drawing. Int.
 
 function getRandomInt(max) {
@@ -9,49 +12,52 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function officeRender(context, color, powerConsumption) {
+function Office(xPos, yPos){
   /*
-    Function to draw an office building sprite. Write once and reuse.
-    context: The context object that will be used to draw the sprite.
-    color: The main color of the sprite.
-    powerConsumption: Amount of power this building is consuming. Int 0-20.
+    Object to handle an office building. Write once and reuse.
    */
+  this.context = context; // The context object to use for drawing.
+  this.x = xPos;  // X coordinate of the sprite.
+  this.y = yPos;  // Y coordinate of the sprite.
+  this.color = "grey";  // Primary color of the sprite.
+  this.consumption = 0;  // Amount of power the building is consuming.
 
-  // Draw walls.
-  this.context.fillStyle(this.color);
-  this.context.fillRect(this.x, this.y, 13 * drawingScale, 12 * drawingScale);
-  // Draw windows.
-  this.context.fillStyle("black");
-  this.context.fillRect(this.x + drawingScale, this.y + drawingScale, 2 * drawingScale, 2 * drawingScale);
-  this.context.fillRect(this.x + (4 * drawingScale), this.y + drawingScale, 2 * drawingScale, 2 * drawingScale);
-  this.context.fillRect(this.x + (7 * drawingScale), this.y + drawingScale, 2 * drawingScale, 2 * drawingScale);
-  this.context.fillRect(this.x + (10 * drawingScale), this.y + drawingScale, 2 * drawingScale, 2 * drawingScale);
-  this.context.fillRect(this.x + drawingScale, this.y + (5 * drawingScale), 2 * drawingScale, 2 * drawingScale);
-  this.context.fillRect(this.x + (4 * drawingScale), this.y + (5 * drawingScale), 2 * drawingScale, 2 * drawingScale);
-  this.context.fillRect(this.x + (7 * drawingScale), this.y + (5 * drawingScale), 2 * drawingScale, 2 * drawingScale);
-  this.context.fillRect(this.x + (10 * drawingScale), this.y + (5 * drawingScale), 2 * drawingScale, 2 * drawingScale);
-  this.context.fillRect(this.x + drawingScale, this.y + (9 * drawingScale), 2 * drawingScale, 2 * drawingScale);
-  this.context.fillRect(this.x + (10 * drawingScale), this.y + (9 * drawingScale), 2 * drawingScale, 2 * drawingScale);
-  // Draw door.
-  this.context.fillStyle("black");
-  this.context.fillRect(this.x + (5 * drawingScale), this.y + (8 * drawingScale), 3 * drawingScale, 4 * drawingScale);
+  this.update = function (){  // Update the state of the object.
+
+  };
+  this.render = function r(){  // Render the sprite.
+    // Draw walls.
+    this.context.fillStyle = this.color;
+    this.context.fillRect(this.x, this.y, 13 * drawingScale, 12 * drawingScale);
+    // Draw windows.
+    this.context.fillStyle = "black";
+    this.context.fillRect(this.x + drawingScale, this.y + drawingScale, 2 * drawingScale, 2 * drawingScale);
+    this.context.fillRect(this.x + (4 * drawingScale), this.y + drawingScale, 2 * drawingScale, 2 * drawingScale);
+    this.context.fillRect(this.x + (7 * drawingScale), this.y + drawingScale, 2 * drawingScale, 2 * drawingScale);
+    this.context.fillRect(this.x + (10 * drawingScale), this.y + drawingScale, 2 * drawingScale, 2 * drawingScale);
+    this.context.fillRect(this.x + drawingScale, this.y + (5 * drawingScale), 2 * drawingScale, 2 * drawingScale);
+    this.context.fillRect(this.x + (4 * drawingScale), this.y + (5 * drawingScale), 2 * drawingScale, 2 * drawingScale);
+    this.context.fillRect(this.x + (7 * drawingScale), this.y + (5 * drawingScale), 2 * drawingScale, 2 * drawingScale);
+    this.context.fillRect(this.x + (10 * drawingScale), this.y + (5 * drawingScale), 2 * drawingScale, 2 * drawingScale);
+    this.context.fillRect(this.x + drawingScale, this.y + (9 * drawingScale), 2 * drawingScale, 2 * drawingScale);
+    this.context.fillRect(this.x + (10 * drawingScale), this.y + (9 * drawingScale), 2 * drawingScale, 2 * drawingScale);
+    // Draw door.
+    this.context.fillStyle = "black";
+    this.context.fillRect(this.x + (5 * drawingScale), this.y + (8 * drawingScale), 3 * drawingScale, 4 * drawingScale);
+  };
 };
 
-var office = kontra.sprite({
-  x: 10,
-  y: 10,
-  color: "grey",
-  powerConsumption: 0,
-
-  render: officeRender(this.context, this.color, this.powerConsumption)
-});
+var office1 = new Office(20, 20);
+var office2 = new Office(100, 20);
 
 var loop = kontra.gameLoop({  // Create the kontra endless game loop.
   update: function(){
     office1.update();
+    office2.update();
   },
   render: function() {
     office1.render();
+    office2.render();
   }
 });
 
